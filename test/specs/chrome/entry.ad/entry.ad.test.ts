47814ab34eda5eabea176cb10f31b12079cf1e87
garik93 <gepard4125@gmail.com>
2023-06-18 18:31:13 +0300

    fixed entry

--- test/specs/chrome/entry.ad/entry.ad.test.ts
+++ test/specs/chrome/entry.ad/entry.ad.test.ts
@@ -1,22 +1,24 @@
 import EntryAdPage from "../../../pageobjects/entry.ad.page";
-import allure, {step} from "@wdio/allure-reporter"
+import {step} from "@wdio/allure-reporter"
 
 describe('Entry Ad', () => {
-    allure.addLabel('Entry Ad', 'BT-1')
     it('entry ad test', async () => {
         await step('open entry ad page', async () => {
             await EntryAdPage.open()
+            await step('open entry ad page', async () => {
+                await EntryAdPage.open()
+            })
         })
 
-        await allure.step('wait For Modal Window Visibility', async () => {
+        await step('wait For Modal Window Visibility', async () => {
             await EntryAdPage.waitForModalWindowVisibility()
         })
 
-        await EntryAdPage.Logger.step('close modal window', async () => {
+        await step('close modal window', async () => {
             await EntryAdPage.closeModal()
         })
 
-        await EntryAdPage.Logger.step('Reopen modal window', async () => {
+        await step('Reopen modal window', async () => {
             await EntryAdPage.reopenModal(1)
         })
 
