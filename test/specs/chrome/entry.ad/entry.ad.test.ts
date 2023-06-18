96905502660c8d043fb76efc4bef45b19d35097b
garik93 <gepard4125@gmail.com>
2023-06-18 19:23:48 +0300

    fixed entry

--- test/specs/chrome/entry.ad/entry.ad.test.ts
+++ test/specs/chrome/entry.ad/entry.ad.test.ts
@@ -1,55 +1,55 @@
 import EntryAdPage from "../../../pageobjects/entry.ad.page";
-import {step} from "@wdio/allure-reporter"
+import {addStep} from "@wdio/allure-reporter"
 
 describe('Entry Ad', () => {
+    it('entry ad test', async () => {
+        await addStep('open entry ad page', async () => {
+            await EntryAdPage.open()
+        })
+
+        await addStep('wait For Modal Window Visibility', async () => {
+            await EntryAdPage.waitForModalWindowVisibility()
+        })
+
+        await addStep('close modal window', async () => {
+            await EntryAdPage.closeModal()
+        })
+
+        await addStep('Reopen modal window', async () => {
+            await EntryAdPage.reopenModal(1)
+        })
+
+        await EntryAdPage.waitForModalWindowVisibility()
+        await EntryAdPage.closeModal()
+    });
+
     // it('entry ad test', async () => {
     //     await step('open entry ad page', async () => {
     //         await EntryAdPage.open()
     //     })
-    //
+    // });
+    // it('wait For Modal Window Visibility', async () => {
+    //     await step('open entry ad page', async () => {
+    //         await EntryAdPage.open()
+    //     })
+    // });
+    // it('wait For Modal Window Visibility', async () => {
     //     await step('wait For Modal Window Visibility', async () => {
     //         await EntryAdPage.waitForModalWindowVisibility()
     //     })
-    //
+    // });
+    // it('close modal window', async () => {
     //     await step('close modal window', async () => {
     //         await EntryAdPage.closeModal()
     //     })
-    //
+    // });
+    // it('Reopen modal window', async () => {
     //     await step('Reopen modal window', async () => {
     //         await EntryAdPage.reopenModal(1)
     //     })
-    //
+    // });
+    // it('entry ad tes', async () => {
     //     await EntryAdPage.waitForModalWindowVisibility()
     //     await EntryAdPage.closeModal()
     // });
-
-    it('entry ad test', async () => {
-        await step('open entry ad page', async () => {
-            await EntryAdPage.open()
-        })
-    });
-    it('wait For Modal Window Visibility', async () => {
-        await step('open entry ad page', async () => {
-            await EntryAdPage.open()
-        })
-    });
-    it('wait For Modal Window Visibility', async () => {
-        await step('wait For Modal Window Visibility', async () => {
-            await EntryAdPage.waitForModalWindowVisibility()
-        })
-    });
-    it('close modal window', async () => {
-        await step('close modal window', async () => {
-            await EntryAdPage.closeModal()
-        })
-    });
-    it('Reopen modal window', async () => {
-        await step('Reopen modal window', async () => {
-            await EntryAdPage.reopenModal(1)
-        })
-    });
-    it('entry ad tes', async () => {
-        await EntryAdPage.waitForModalWindowVisibility()
-        await EntryAdPage.closeModal()
-    });
 })
